240fc4e7d2bae73c95658a8d668f0afe1e97f647
loressl <loryssl@hotmail.com>
2019-03-11 14:15:20 -0300

    Remoção dos publics.
          *Foi feita excluisão dos publics da interface, pois o software sinalinazou que não há necessidade.

--- app/src/main/java/com/example/maqui/bancodedadossqlite/Helper/ITarefaDAO.java
+++ app/src/main/java/com/example/maqui/bancodedadossqlite/Helper/ITarefaDAO.java
@@ -9,5 +9,5 @@ public interface ITarefaDAO {
     boolean salvar(Tarefa tarefa);
     boolean atualizar(Tarefa tarefa);
     boolean deletar(Tarefa tarefa);
-    List<Tarefa> listar();
+    public List<Tarefa> listar();
 }
